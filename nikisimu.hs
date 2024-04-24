ab74ebdb7d585c519e339ef599a8d009dcae4fa4
Nikilamblot <165933083+Nikilamblot@users.noreply.github.com>
2024-04-24 14:01:16 -0300

    Add files via upload

---
+++ nikisimu.hs
@@ -0,0 +1,37 @@
+-- Ejercicio 1
+relacionesValidas :: [(String, String)] -> Bool
+relacionesValidas [] = True
+relacionesValidas ((a,b):xs) | a == b = False
+                                   | pertenece (a,b) xs = False
+                                   | otherwise = relacionesValidas xs
+
+tuplasIguales :: (String, String) -> (String, String) -> Bool
+tuplasIguales (a,b) (c,d) | a == d && b == c = True
+                          | a == c && b == d = True
+                          | otherwise = False
+
+pertenece :: (String, String) -> [(String, String)] -> Bool
+pertenece _ [] = False
+pertenece (a,b) ((x,y):xs) | tuplasIguales (a,b) (x,y) = True
+                           | otherwise = pertenece (a,b) xs
+
+--------------------------------
+personas :: [(String, String)] -> [String]
+personas [] = []
+personas ((a,b):xs) = quitarrepetidos (a : b : personas xs)
+
+quitarrepetidos :: [String] -> [String]
+quitarrepetidos [] = []
+quitarrepetidos (x:xs) | x == head xs = quitarrepetidos xs
+                       | otherwise = head xs : quitarUno xs
+
+quitarUno :: [String] -> [String]
+quitarUno [] = []
+quitarUno [x] = [x]
+quitarUno (x:xs) | x == head xs = xs
+                 | otherwise = x : quitarUno xs
+
+{--pertenece2 :: String -> [(String, String)] -> Bool
+pertenece2 _ [] = False
+pertenece2 e (x:xs) | e == x = True
+                    | otherwise = pertenece2 e xs--}
